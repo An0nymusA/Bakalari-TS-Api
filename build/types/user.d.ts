8d8d031107e225b76203166d1f479691e8b98a95
Adam Zvěřina <adam.zverina@gmail.com>
2024-01-23 16:59:09 +0100

    user type tweaks

--- build/types/user.d.ts
+++ build/types/user.d.ts
@@ -1,33 +1,41 @@
 export interface User {
     UserUID: string;
-    Class: UserClass;
+    CampaignCategoryCode: string;
+    Class: Class;
     FullName: string;
     SchoolOrganizationName: string;
-    SchoolType: string;
+    SchoolType: string | null;
     UserType: string;
     UserTypeText: string;
     StudyYear: number;
-    EnabledModules: Array<EnabledModule>;
-    SettingModules: SettingModule;
+    EnabledModules: EnabledModule[];
+    SettingModules: SettingModules;
+    FullUserName: string;
+    Students: Student[];
 }
-export interface UserClass {
+export interface Class {
     Id: string;
     Abbrev: string;
     Name: string;
 }
 export interface EnabledModule {
     Module: string;
-    Rights: Array<string>;
+    Rights: string[];
 }
-export interface SettingModule {
-    Common: Common;
+export interface SettingModules {
+    [key: string]: {
+        $type: string;
+        UploadFolderVerified?: boolean;
+        ActualSemester?: {
+            SemesterId: string;
+            From: string;
+            To: string;
+        };
+    };
 }
-export interface Common {
-    $type: string;
-    ActualSemester: ActualSemester;
-}
-export interface ActualSemester {
-    SemesterId: string;
-    From: string;
-    To: string;
+export interface Student {
+    FullName: string;
+    Class: Class;
+    SchoolType: string | null;
+    StudyYear: number;
 }
